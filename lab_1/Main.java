c66efb4cba325f8a40c352a7da002ff9e23428bb
ArchikTheSLayer <48150282+ArchikTheSLayer@users.noreply.github.com>
2019-09-15 18:43:19 +0300

    Add files via upload

---
+++ lab_1/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+import Calculations.Calculations;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculations calc = new Calculations();
+        System.out.println(calc.calculate());
+    }
+}
